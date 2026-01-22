7c25fe7e0b8887a6bf770f7be0fe7c4fec38b8e3
sth-s <stefan.shushpanov@gmail.com>
2026-01-22 22:41:13 +0100

    tests upd

--- tests/unit/test_service.py
+++ tests/unit/test_service.py
@@ -2,17 +2,16 @@ import pytest
 from pathlib import Path
 from unittest.mock import Mock
 from theater.application.statement_service import StatementService
-from theater.infrastructure.repository import Repository
+from theater.infrastructure.repositories.repository_interface import RepositoryInterface
 from theater.infrastructure.text_renderer import TextRenderer
 from theater.domain.statements_generator_service import StatementsGeneratorService
 from theater.models.invoice import Invoice, Performance
 from theater.models.play import Play
-from theater.models.rules import PricingRules, AmountRules, CreditsRules
 
 
 @pytest.fixture
 def mock_repository():
-    repo = Mock(spec=Repository)
+    repo = Mock(spec=RepositoryInterface)
     
     invoices = [
         Invoice(
@@ -29,38 +28,8 @@ def mock_repository():
         "as-like": Play(play_id="as-like", name="As You Like It", type="comedy")
     }
     
-    rules = {
-        "tragedy": PricingRules(
-            amount=AmountRules(
-                base_amount=40000,
-                base_coefficient=0,
-                audience_threshold_amount=30,
-                threshold_amount=0,
-                threshold_coefficient=1000
-            ),
-            credits=CreditsRules(
-                audience_threshold_credits=30,
-                credits_divisor=None
-            )
-        ),
-        "comedy": PricingRules(
-            amount=AmountRules(
-                base_amount=30000,
-                base_coefficient=300,
-                audience_threshold_amount=20,
-                threshold_amount=10000,
-                threshold_coefficient=500
-            ),
-            credits=CreditsRules(
-                audience_threshold_credits=32,
-                credits_divisor=5
-            )
-        )
-    }
-    
     repo.get_invoices.return_value = invoices
     repo.get_plays_dict.return_value = plays
-    repo.get_pricing_rules_dict.return_value = rules
     
     return repo
 
